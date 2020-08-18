f9db659b99094de2c7e0b2eb77567e2af899db27
Pablo Company Ramírez <p.c.ramirez5150@gmail.com>
2020-08-18 12:58:44 +0200

    Add remove values from linked list code challenge

---
+++ CodeChallenges.RemoveValuesFromLinkedlist/LinkedListNode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CodeChallenges.RemoveValuesFromLinkedlist
+{
+    public class LinkedListNode
+    {
+        public int Val { get; }
+        public LinkedListNode Next { get; set; }
+
+        public LinkedListNode(int value)
+        {
+            Val = value;
+        }
+
+        public void AppendNode(int value)
+        {
+            var prev = this;
+            var tmp = Next;
+            while (tmp != null)
+            {
+                prev = tmp;
+                tmp = tmp.Next;
+            }
+
+            prev.Next = new LinkedListNode(value);
+        }
+
+        public void Print()
+        {
+            var node = this;
+            while (node != null)
+            {
+                Console.Write(node.Val + ", ");
+                node = node.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ CodeChallenges.RemoveValuesFromLinkedlist/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace CodeChallenges.RemoveValuesFromLinkedlist
+{
+    class Program
+    {
+        static void Main()
+        {
+            var head = new LinkedListNode(3);
+            head.AppendNode(3);
+            head.AppendNode(3);
+            head.AppendNode(1);
+            head.AppendNode(5);
+            head.AppendNode(2);
+            head.AppendNode(3);
+            head.AppendNode(3);
+            head.AppendNode(3);
+            head.AppendNode(1);
+            head.AppendNode(4);
+            head.AppendNode(3);
+            head.AppendNode(3);
+
+            var removedLinkedList = RemoveDigits(3, head);
+
+            removedLinkedList.Print();
+        }
+
+        static LinkedListNode RemoveDigits(int value, LinkedListNode firstNode)
+        {
+            if (firstNode == null)
+                return null;
+
+            while (firstNode.Val == value)
+                firstNode = firstNode.Next;
+
+            LinkedListNode previousNode = null;
+            var currentNode = firstNode;
+            while (currentNode != null)
+            {
+                if (currentNode.Val == value)
+                    previousNode.Next = currentNode.Next;
+                else
+                    previousNode = currentNode;
+
+                currentNode = currentNode.Next;
+            }
+
+            return firstNode;
+        }
+    }
+}
