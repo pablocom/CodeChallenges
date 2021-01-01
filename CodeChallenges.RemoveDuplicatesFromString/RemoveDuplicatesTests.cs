fa996ca3d02f5a1c2402b3ebd3d948c645d65807
Pablo Company Ramírez <p.c.ramirez5150@gmail.com>
2021-01-01 16:55:27 +0100

    Adds remove duplicate letters code challenge

---
+++ CodeChallenges.RemoveDuplicatesFromString/RemoveDuplicatesTests.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+
+namespace CodeChallenges.RemoveDuplicatesFromString
+{
+    public class RemoveDuplicatesTests
+    {
+        [Test]
+        public void Test1()
+        {
+            var text = "bcabc";
+
+            var result = new Solution().RemoveDuplicateLetters(text);
+
+            Assert.That(result, Is.EqualTo("abc"));
+        } 
+        
+        [Test]
+        public void Test2()
+        {
+            var text = "cbacdcbc";
+
+            var result = new Solution().RemoveDuplicateLetters(text);
+
+            Assert.That(result, Is.EqualTo("acdb"));
+        }
+    }
+}
---
+++ CodeChallenges.RemoveDuplicatesFromString/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeChallenges.RemoveDuplicatesFromString
+{
+    public class Solution
+    {
+        public string RemoveDuplicateLetters(string s)
+        {
+            var occurrencesByLetter = new Dictionary<char, int>();
+            for (var letter = 'a'; letter <= 'z'; letter++)
+            {
+                var occurrences = s.Count(character => character == letter);
+                occurrencesByLetter.Add(letter, occurrences);
+            }
+
+            var resultString = s;
+            for (var letter = 'z'; letter >= 'a'; letter--) 
+            {
+                while (occurrencesByLetter[letter] > 1)
+                {
+                    resultString = RemoveFirstChar(resultString, letter);
+                    occurrencesByLetter[letter]--;
+                }
+
+            }
+
+            return resultString;
+        }
+
+        public string RemoveFirstChar(string text, char letter)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == letter)
+                    return text.Remove(i, 1);
+            }
+
+            return text;
+        }
+    }
+}
