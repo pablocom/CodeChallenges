3ae803bf9ec76be516c11d3d99db8d220f644987
Pablo Company Ramírez <p.c.ramirez5150@gmail.com>
2021-01-06 20:40:30 +0100

    Creates LongestCommonSubsequence project

---
+++ CodeChallenges.LongestCommonSubsequence/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace CodeChallenges.LongestCommonSubsequence
+{
+    public class LongestCommonSubsequenceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
