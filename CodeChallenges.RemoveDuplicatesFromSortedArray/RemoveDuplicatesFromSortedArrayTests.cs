4e2298a9677cbdd3f6152d0cf10a664407a39c18
Pablo Company Ramírez <p.c.ramirez5150@gmail.com>
2021-01-20 19:12:14 +0100

    Adds Remove duplicates from array

---
+++ CodeChallenges.RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArrayTests.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+
+namespace CodeChallenges.RemoveDuplicatesFromSortedArray
+{
+    public class RemoveDuplicatesFromSortedArrayTests
+    {
+        [Test]
+        public void Test1()
+        {
+            var array = new[] {1, 2, 2};
+
+            var resultArrayLength = new Solution().RemoveDuplicates(array);
+            
+            Assert.That(resultArrayLength, Is.EqualTo(2));
+            Assert.That(array[0], Is.EqualTo(1));
+            Assert.That(array[1], Is.EqualTo(2));
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            var array = new[] {1, 2, 2, 3};
+
+            var resultArrayLength = new Solution().RemoveDuplicates(array);
+            
+            Assert.That(resultArrayLength, Is.EqualTo(3));
+            Assert.That(array[0], Is.EqualTo(1));
+            Assert.That(array[1], Is.EqualTo(2));
+            Assert.That(array[2], Is.EqualTo(3));
+        }
+    }
+}
---
+++ CodeChallenges.RemoveDuplicatesFromSortedArray/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace CodeChallenges.RemoveDuplicatesFromSortedArray
+{
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] array)
+        {
+            if (array == null || array.Length == 0)
+                return 0;
+            
+            var positionToAdd = 1;
+
+            for (var i = 1; i < array.Length; i++)
+            {
+                if (array[i - 1] != array[i])
+                    array[positionToAdd++] = array[i];
+            }
+            
+            return positionToAdd;
+        }
+    }
+}
