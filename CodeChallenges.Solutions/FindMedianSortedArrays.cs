05af7f7370b768cf6eb35f239138e232c528748a
Pablo Company <pablo.companyramirez@agicap.com>
2022-05-29 23:15:19 +0200

    Add FindMedianSortedArrays

---
+++ CodeChallenges.Solutions/FindMedianSortedArrays.cs
@@ -0,0 +1,89 @@
+using System.Linq;
+
+namespace CodeChallenges.Solutions;
+
+public class FindMedianSortedArrays 
+{
+    public double Solve(int[] nums1, int[] nums2)
+    {
+        if (!nums2.Any())
+        {
+            var middlePosition = nums1.Length / 2;
+            if (nums1.Length % 2 == 0)
+            {
+                return (double)(nums1[middlePosition - 1] + nums1[middlePosition]) / 2;
+            }
+            
+            return nums1[middlePosition];
+        }
+        
+        if (!nums1.Any())
+        {
+            var middlePosition = nums2.Length / 2;
+            if (nums2.Length % 2 == 0)
+            {
+                return (double)(nums2[middlePosition - 1] + nums2[middlePosition]) / 2;
+            }
+            
+            return nums2[middlePosition];
+        }
+        
+        var numbersAtLeftOfMedianCount = 0;
+        var numbersAtRightOfMedianCount = nums1.Length + nums2.Length;
+
+        var lastIterationWasInOne = false;
+        int iteratorOne = 0, iteratorTwo = 0;
+        while (numbersAtLeftOfMedianCount < numbersAtRightOfMedianCount)
+        {
+            numbersAtLeftOfMedianCount++;
+            numbersAtRightOfMedianCount--;
+            
+            if (iteratorOne != nums1.Length && nums1[iteratorOne] <= nums2[iteratorTwo])
+            {
+                iteratorOne++;
+                lastIterationWasInOne = true;
+                continue;
+            }
+
+            iteratorTwo++;
+            lastIterationWasInOne = false;
+        }
+
+        if (MergedArraysCountIsEven(nums1, nums2))
+        {
+            if (lastIterationWasInOne)
+            {
+                var isAtLastPositionOfArray = iteratorOne == nums1.Length;
+                if (isAtLastPositionOfArray)
+                    return (double)(nums1[iteratorOne - 1] + nums2[iteratorTwo]) / 2;
+                
+                var nextOnOne = nums1[iteratorOne];
+                if (nextOnOne >= nums2[iteratorTwo])
+                {
+                    return (double)(nums1[iteratorOne - 1] + nums2[iteratorTwo]) / 2;
+                }
+            }
+            else
+            {
+                if (iteratorOne >= nums1.Length)
+                    return (double)(nums2[iteratorTwo - 1] + nums2[iteratorTwo]) / 2;
+                
+                var nextOnTwo = nums2[iteratorTwo];
+                if (nextOnTwo >= nums1[iteratorOne])
+                {
+                    return (double)(nums2[iteratorTwo - 1] + nums1[iteratorOne]) / 2;
+                }
+            }
+        }
+
+        if (lastIterationWasInOne)
+            return nums1[iteratorOne - 1];
+        
+        return nums2[iteratorTwo - 1];
+    }
+
+    private static bool MergedArraysCountIsEven(int[] nums1, int[] nums2)
+    {
+        return (nums1.Length + nums2.Length) % 2 == 0;
+    }
+}
---
+++ CodeChallenges.UnitTests/FindMedianSortedArraysTests.cs
@@ -0,0 +1,151 @@
+using System;
+using CodeChallenges.Solutions;
+using NUnit.Framework;
+
+namespace CodeChallenges.UnitTests;
+
+public class FindMedianSortedArraysTests
+{
+    [Test]
+    public void Test1()
+    {
+        var firstArray = new[] { 1, 3 };
+        var secondArray = new[] { 2 };
+
+        var median = new FindMedianSortedArrays().Solve(firstArray, secondArray);
+        
+        Assert.That(median, Is.EqualTo(2d));
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        var firstArray = new[] { 1, 2, 3 };
+        var secondArray = new[] { 4, 5 };
+
+        var median = new FindMedianSortedArrays().Solve(firstArray, secondArray);
+        
+        Assert.That(median, Is.EqualTo(3d));
+    }
+    
+    [Test]
+    public void Test3()
+    {
+        var firstArray = new[] { 1, 2 };
+        var secondArray = new[] { 2, 4 };
+
+        var median = new FindMedianSortedArrays().Solve(firstArray, secondArray);
+        
+        Assert.That(median, Is.EqualTo(2d));
+    }
+
+    [Test]
+    public void Test4()
+    {
+        var firstArray = new[] { 1, 2 };
+        var secondArray = new[] { 2, 3 };
+
+        var median = new FindMedianSortedArrays().Solve(firstArray, secondArray);
+        
+        Assert.That(median, Is.EqualTo(2.0d));
+    }
+    
+    [Test]
+    public void Test5()
+    {
+        var firstArray = new[] { 1, 3 };
+        var secondArray = new[] { 2, 4, 5 };
+
+        var median = new FindMedianSortedArrays().Solve(firstArray, secondArray);
+        
+        Assert.That(median, Is.EqualTo(3.0d));
+    }
+    
+    [Test]
+    public void Test6()
+    {
+        var firstArray = new[] { 1 };
+        var secondArray = new[] { 3, 3, 4, 5, 6 };
+
+        var median = new FindMedianSortedArrays().Solve(firstArray, secondArray);
+        
+        Assert.That(median, Is.EqualTo(3.5d));
+    }
+    
+    [Test]
+    public void Test7()
+    {
+        var firstArray = new[] { 1, 2, 3, 6, 7 };
+        var secondArray = new[] { 4 };
+
+        var median = new FindMedianSortedArrays().Solve(firstArray, secondArray);
+        
+        Assert.That(median, Is.EqualTo(3.5d));
+    }
+    
+    [Test]
+    public void Test8()
+    {
+        var firstArray = new[] { 1, 2, 3, 6, 7 };
+        var secondArray = Array.Empty<int>();
+
+        var median = new FindMedianSortedArrays().Solve(firstArray, secondArray);
+        
+        Assert.That(median, Is.EqualTo(3d));
+    }
+    
+    [Test]
+    public void Test9()
+    {
+        var firstArray = Array.Empty<int>();
+        var secondArray = new[] { 1, 2, 3, 4 };
+
+        var median = new FindMedianSortedArrays().Solve(firstArray, secondArray);
+        
+        Assert.That(median, Is.EqualTo(2.5d));
+    }
+    
+    [Test]
+    public void Test10()
+    {
+        var firstArray = new[] {1, 3};
+        var secondArray = new[] { 2, 7 };
+
+        var median = new FindMedianSortedArrays().Solve(firstArray, secondArray);
+        
+        Assert.That(median, Is.EqualTo(2.5d));
+    }
+    
+    [Test]
+    public void Test11()
+    {
+        var firstArray = new[] {2, 3};
+        var secondArray = Array.Empty<int>();
+
+        var median = new FindMedianSortedArrays().Solve(firstArray, secondArray);
+        
+        Assert.That(median, Is.EqualTo(2.5d));
+    }
+    
+    [Test]
+    public void Test12()
+    {
+        var firstArray = new[] {2, 2, 4, 4};
+        var secondArray = new[] {2, 2, 4, 4};
+
+        var median = new FindMedianSortedArrays().Solve(firstArray, secondArray);
+        
+        Assert.That(median, Is.EqualTo(3d));
+    }
+    
+    [Test]
+    public void Test13()
+    {
+        var firstArray = new[] {1, 2};
+        var secondArray = new[] {-1, 3};
+
+        var median = new FindMedianSortedArrays().Solve(firstArray, secondArray);
+        
+        Assert.That(median, Is.EqualTo(3d));
+    }
+}
