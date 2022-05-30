c2cc0e2bb401ce6e8653377183eb7d9695fd8ed9
Pablo <p.c.ramirez5150@gmail.com>
2022-05-30 22:54:42 +0200

    Add SmallestNonPresentPositiveNumber

---
+++ CodeChallenges.Solutions/SmallestNonPresentPositiveNumber.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace CodeChallenges.Solutions
+{
+    public class SmallestNonPresentPositiveNumber
+    {
+        public int Solution(int[] A)
+        {
+            var numbersPresent = new HashSet<int>();
+
+            for (int i = 1; i < 1000000; i++)
+            {
+                if (!numbersPresent.Contains(i))
+                {
+                    return i;
+                }
+            }
+
+            return 1000001;
+        }
+    }
+}
---
+++ CodeChallenges.UnitTests/SmallestNonPresentPositiveNumberTests.cs
@@ -0,0 +1,19 @@
+﻿
+using CodeChallenges.Solutions;
+using NUnit.Framework;
+
+namespace CodeChallenges.UnitTests
+{
+    public class SmallestNonPresentPositiveNumberTests
+    {
+        [Test]
+        public void Test1()
+        {
+            var array = new[] { 1, 3, 6, 4, 1, 2 };
+
+            var solution = new SmallestNonPresentPositiveNumber().Solution(array);
+
+            Assert.That(solution, Is.EqualTo(5));
+        }
+    }
+}
