088ee385fa6710663263267e6259257b05cdc1f8
Pablo <p.c.ramirez5150@gmail.com>
2022-05-31 13:06:17 +0200

    Adds ProductOfArrayExceptItself

---
+++ CodeChallenges.Solutions/ProductOfArrayExceptItself.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CodeChallenges.Solutions;
+
+// Description link: https://leetcode.com/problems/product-of-array-except-self/
+public class ProductOfArrayExceptItself
+{
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        var accumulatedProductsLeftToRight = new int[nums.Length];
+        accumulatedProductsLeftToRight[0] = nums[0];
+        var accumulatedProductsIterator = 1;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            accumulatedProductsLeftToRight[accumulatedProductsIterator] = accumulatedProductsLeftToRight[i - 1] * nums[i];
+            accumulatedProductsIterator++;
+        }
+
+        var accumulatedProductsRightToLeft = new int[nums.Length];
+        accumulatedProductsRightToLeft[nums.Length - 1] = nums[nums.Length - 1];
+        accumulatedProductsIterator = nums.Length - 2;
+        for (int i = nums.Length - 2; i >= 0; i--)
+        {
+            accumulatedProductsRightToLeft[accumulatedProductsIterator] = accumulatedProductsRightToLeft[i + 1] * nums[i];
+            accumulatedProductsIterator--;
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (i == 0)
+            {
+                nums[i] = accumulatedProductsRightToLeft[i + 1];
+                continue;
+            }
+            if (i == nums.Length - 1)
+            {
+                nums[i] = accumulatedProductsLeftToRight[i - 1];
+                continue;
+            }
+
+            nums[i] = accumulatedProductsLeftToRight[i - 1] * accumulatedProductsRightToLeft[i + 1];
+        }
+
+        return nums;
+    }
+}
---
+++ CodeChallenges.UnitTests/ProductOfArrayExceptItselfTests.cs
@@ -0,0 +1,18 @@
+﻿using CodeChallenges.Solutions;
+using NUnit.Framework;
+
+namespace CodeChallenges.UnitTests
+{
+    public class ProductOfArrayExceptItselfTests
+    {
+        [Test]
+        public void Test1()
+        {
+            var nums = new int[] { 1, 2, 3, 4 };
+
+            var solution = new ProductOfArrayExceptItself().ProductExceptSelf(nums);
+
+            CollectionAssert.AreEquivalent(new int[] { 24, 12, 8, 6 }, solution);
+        }
+    }
+}
