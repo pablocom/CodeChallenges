5496d60e94fad0ee35b0a7610c96f85f15bff821
Pablo <p.c.ramirez5150@gmail.com>
2022-05-31 13:18:20 +0200

    Refactor ProductOfArrayExceptItself

--- CodeChallenges.Solutions/ProductOfArrayExceptItself.cs
+++ CodeChallenges.Solutions/ProductOfArrayExceptItself.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace CodeChallenges.Solutions;
+﻿namespace CodeChallenges.Solutions;
 
 // Description link: https://leetcode.com/problems/product-of-array-except-self/
 public class ProductOfArrayExceptItself
@@ -9,35 +7,23 @@ public class ProductOfArrayExceptItself
     {
         var accumulatedProductsLeftToRight = new int[nums.Length];
         accumulatedProductsLeftToRight[0] = nums[0];
-        var accumulatedProductsIterator = 1;
         for (int i = 1; i < nums.Length; i++)
         {
-            accumulatedProductsLeftToRight[accumulatedProductsIterator] = accumulatedProductsLeftToRight[i - 1] * nums[i];
-            accumulatedProductsIterator++;
+            accumulatedProductsLeftToRight[i] = accumulatedProductsLeftToRight[i - 1] * nums[i];
         }
 
         var accumulatedProductsRightToLeft = new int[nums.Length];
         accumulatedProductsRightToLeft[nums.Length - 1] = nums[nums.Length - 1];
-        accumulatedProductsIterator = nums.Length - 2;
         for (int i = nums.Length - 2; i >= 0; i--)
         {
-            accumulatedProductsRightToLeft[accumulatedProductsIterator] = accumulatedProductsRightToLeft[i + 1] * nums[i];
-            accumulatedProductsIterator--;
+            accumulatedProductsRightToLeft[i] = accumulatedProductsRightToLeft[i + 1] * nums[i];
         }
 
-        for (int i = 0; i < nums.Length; i++)
-        {
-            if (i == 0)
-            {
-                nums[i] = accumulatedProductsRightToLeft[i + 1];
-                continue;
-            }
-            if (i == nums.Length - 1)
-            {
-                nums[i] = accumulatedProductsLeftToRight[i - 1];
-                continue;
-            }
+        nums[0] = accumulatedProductsRightToLeft[1];
+        nums[nums.Length - 1] = accumulatedProductsLeftToRight[nums.Length - 2];
 
+        for (int i = 1; i < nums.Length - 1; i++)
+        {
             nums[i] = accumulatedProductsLeftToRight[i - 1] * accumulatedProductsRightToLeft[i + 1];
         }
 
