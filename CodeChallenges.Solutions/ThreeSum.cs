74bcdc0bebf58702e4f39558b5aa9c945edc936a
Pablo <p.c.ramirez5150@gmail.com>
2022-06-10 22:20:31 +0200

    Adds sub optimal solution for ThreeSum

---
+++ CodeChallenges.Solutions/ThreeSum.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeChallenges.Solutions;
+
+public class ThreeSum
+{
+    public ThreeSum()
+    {
+    }
+
+    public IList<IList<int>> Solve(int[] nums)
+    {
+        Array.Sort(nums);
+
+        var result = new List<IList<int>>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (i > 0 && nums[i - 1] == nums[i])
+                continue;
+
+            var left = i + 1;
+            var right = nums.Length - 1;
+
+            while (left < right)
+            {
+                var sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0)
+                {
+                    result.Add(new List<int>() { nums[i], nums[left], nums[right] });
+                    while (left < right && nums[left] == nums[left + 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right - 1])
+                        right--;
+                    left++;
+                    right--;
+                }
+                else if (sum < 0)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
+// [-1,  0, 1, 2, -1, -4]
+// [-1, -1, 0, 2,  1, -3]
---
+++ CodeChallenges.UnitTests/ThreeSumTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeChallenges.Solutions;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace CodeChallenges.UnitTests;
+
+public class ThreeSumTests
+{
+    [Test]
+    public void Test1()
+    {
+        var inputArray = new[] { -1, 0, 1, 2, -1, -4 };
+
+        IList<IList<int>> solution = new ThreeSum().Solve(inputArray);
+
+        CollectionAssert.AreEquivalent(
+            new List<int[]>() { new[] { -1, -1, 2 },new[] { -1, 0, 1 }},
+            solution
+        );
+    }
+}
