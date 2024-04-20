494d8f0915cbd4884bf64e332f03857ee1e3d584
Pablo Company <p.c.ramirez5150@gmail.com>
2024-04-20 13:38:31 +0200

    Adds divide two integers solution

---
+++ CodeChallenges.Solutions/DivideTwoIntegers.cs
@@ -0,0 +1,33 @@
+﻿namespace CodeChallenges.Solutions;
+
+public sealed class DivideTwoIntegers
+{
+    public int Divide(int dividend, int divisor)
+    {
+        if (dividend == int.MinValue && divisor == -1)
+            return int.MaxValue;
+
+        if (dividend == int.MinValue && divisor == 1)
+            return int.MinValue;
+
+        var dividendIsNegative = dividend < 0;
+        var divisorIsNegative = divisor < 0;
+        var sign = (dividendIsNegative && !divisorIsNegative) || (!dividendIsNegative && divisorIsNegative) ? -1 : 1;
+        
+        var absDividend = Math.Abs((long)dividend);
+        var absDivisor = Math.Abs((long)divisor);
+        var quotient = 0;
+        long currentMultipliedValue = 0;
+
+        while (currentMultipliedValue < absDividend) 
+        {
+            currentMultipliedValue += absDivisor;
+            quotient++;
+        }
+
+        if (currentMultipliedValue > absDividend)
+            quotient--;
+
+        return quotient * sign;
+    }
+}
---
+++ CodeChallenges.UnitTests/DivideTwoIntegersTests.cs
@@ -0,0 +1,41 @@
+﻿using CodeChallenges.Solutions;
+
+namespace CodeChallenges.UnitTests;
+
+public class DivideTwoIntegersTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var result = new DivideTwoIntegers().Divide(10, 5);
+        result.Should().Be(2);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = new DivideTwoIntegers().Divide(16, 5);
+        result.Should().Be(3);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var result = new DivideTwoIntegers().Divide(7, -3);
+        result.Should().Be(-2);
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var result = new DivideTwoIntegers().Divide(-2147483648, -1);
+        result.Should().Be(2147483647);
+    }
+
+    [Fact]
+    public void Test5()
+    {
+        var result = new DivideTwoIntegers().Divide(2147483647, 2147483646);
+        result.Should().Be(1);
+    }
+}
