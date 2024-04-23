8524f159ceafaa779479fac6c0a452d7bf4a6676
Pablo Company <p.c.ramirez5150@gmail.com>
2024-04-23 23:23:00 +0200

    WIP - SearchInRotatedArray

---
+++ CodeChallenges.Solutions/SearchInRotatedArray.cs
@@ -0,0 +1,36 @@
+﻿namespace CodeChallenges.Solutions;
+
+public class SearchInRotatedArray
+{
+    public int Search(int[] nums, int target)
+    {
+        var left = 0;
+        var right = nums.Length - 1;
+
+        // [3, 4, 0, 1, 2]
+        while (left <= right)
+        {
+            var mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[left] <= nums[mid])
+            {
+                if (nums[left] <= target && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+            else
+            {
+                if (nums[mid] < target && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ CodeChallenges.UnitTests/SearchInRotatedArrayTests.cs
@@ -0,0 +1,17 @@
+﻿using CodeChallenges.Solutions;
+
+namespace CodeChallenges.UnitTests;
+
+public class SearchInRotatedArrayTests
+{
+    [Fact]
+    public void Test1()
+    {
+        int[] nums = [3, 4, 0, 1, 2];
+        var target = 1;
+
+        var solution = new SearchInRotatedArray().Search(nums, target);
+
+        solution.Should().Be(3);
+    }
+}
