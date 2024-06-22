98676f6c93bd73fff956f4513afd08b1cd753e92
Pablo Company <p.c.ramirez5150@gmail.com>
2024-06-22 18:07:21 +0200

    Adds LowestCommonAncestorInBinaryTree

---
+++ CodeChallenges.Solutions/LowestCommonAncestorInBinaryTree.cs
@@ -0,0 +1,23 @@
+﻿using TreeNode = CodeChallenges.Solutions.BinaryTrees.LeetCodeTreeNode;
+
+namespace CodeChallenges.Solutions;
+
+public sealed class LowestCommonAncestorInBinaryTree
+{
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    {
+        if (root is null)
+            return null!;
+
+        if (root.val == p.val || root.val == q.val) 
+            return root;
+
+        var left = LowestCommonAncestor(root.left, p, q);
+        var right = LowestCommonAncestor(root.right, p, q);
+        
+        if (left is not null && right is not null) 
+            return root;
+        
+        return left ?? right!;
+    }
+}
---
+++ CodeChallenges.UnitTests/LowestCommonAncestorInBinaryTreeTests.cs
@@ -0,0 +1,64 @@
+﻿using CodeChallenges.Solutions;
+using CodeChallenges.Solutions.BinaryTrees;
+
+namespace CodeChallenges.UnitTests;
+
+public sealed class LowestCommonAncestorInBinaryTreeTests
+{
+    [Fact]
+    public void Test1()
+    {
+        LeetCodeTreeNode bstRoot = new(3);
+
+        bstRoot.left = new(5);
+        bstRoot.right = new(1);
+
+        bstRoot.left.left = new(6);
+        bstRoot.left.right = new(2);
+        bstRoot.left.right.left = new(7);
+        bstRoot.left.right.right = new(4);
+
+        bstRoot.right = new(1);
+        bstRoot.right.left = new(0);
+        bstRoot.right.right = new(8);
+
+        var lowestCommonAncestor = new LowestCommonAncestorInBinaryTree().LowestCommonAncestor(bstRoot, bstRoot.left, bstRoot.right);
+
+        lowestCommonAncestor.Should().Be(bstRoot);
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        LeetCodeTreeNode bstRoot = new(3);
+
+        bstRoot.left = new(5);
+
+        bstRoot.left.left = new(6);
+        bstRoot.left.right = new(2);
+        bstRoot.left.right.left = new(7);
+        bstRoot.left.right.right = new(4);
+
+        bstRoot.right = new(1);
+        bstRoot.right.left = new(0);
+        bstRoot.right.right = new(8);
+
+        var lowestCommonAncestor = new LowestCommonAncestorInBinaryTree().LowestCommonAncestor(bstRoot, bstRoot.left.left, bstRoot.left.right);
+
+        lowestCommonAncestor.Should().Be(bstRoot.left);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        LeetCodeTreeNode bstRoot = new(3);
+
+        bstRoot.right = new(1);
+        bstRoot.right.left = new(0);
+        bstRoot.right.right = new(8);
+
+        var lowestCommonAncestor = new LowestCommonAncestorInBinaryTree().LowestCommonAncestor(bstRoot, bstRoot.right.left, bstRoot.right.right);
+
+        lowestCommonAncestor.Should().Be(bstRoot.right);
+    }
+}
