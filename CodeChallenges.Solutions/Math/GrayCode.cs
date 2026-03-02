bbe538eeb7d8105802e4b2e91d9590de82b75d23
Pablo Company <p.c.ramirez5150@gmail.com>
2026-03-02 14:38:28 +0100

    Add gray code sequence

---
+++ CodeChallenges.Solutions/Math/GrayCode.cs
@@ -0,0 +1,19 @@
+namespace CodeChallenges.Solutions.Math;
+
+public static class GrayCode
+{
+    public static IList<int> Solve(int n)
+    {
+        var result = new List<int> { 0 };
+        
+        for (var i = 0; i < n; i++) {
+            var addition = 1 << i;
+            
+            for (var j = result.Count - 1; j >= 0; j--) {
+                result.Add(result[j] + addition);
+            }
+        }
+        
+        return result;
+    }
+}
---
+++ CodeChallenges.UnitTests/Math/GrayCodeTests.cs
@@ -0,0 +1,21 @@
+using CodeChallenges.Solutions.Math;
+
+namespace CodeChallenges.UnitTests.Math;
+
+public class GrayCodeTests
+{
+    [Theory]
+    [InlineData(0, new[] { 0 })]
+    [InlineData(1, new[] { 0, 1 })]
+    [InlineData(2, new[] { 0, 1, 3, 2 })]
+    [InlineData(3, new[] { 0, 1, 3, 2, 6, 7, 5, 4 })]
+    [InlineData(4, new[] { 0, 1, 3, 2, 6, 7, 5, 4, 12, 13, 15, 14, 10, 11, 9, 8 })]
+    public void Tests(int n, int[] expected)
+    {
+        var result = GrayCode.Solve(n);
+
+        
+        
+        result.Should().Equal(expected);
+    }
+}
